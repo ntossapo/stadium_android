d849efeac6b02c0dc503cffba76be0931a78f8e9
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-01-29 13:52:16 +0700

    change smth in MainActivity but i don't remember

--- app/src/main/java/com/tossapon/stadiumfinder/Network/Server.java
+++ app/src/main/java/com/tossapon/stadiumfinder/Network/Server.java
@@ -4,5 +4,5 @@ package com.tossapon.stadiumfinder.Network;
  * Created by Tossapon on 20/12/2558.
  */
 public class Server {
-    public static final String BASEURL = "http://192.168.1.2:8000";
+    public static final String BASEURL = "http://192.168.56.1:8000";
 }
